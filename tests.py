eb0d87ec049ad738ce58097efb7dddb262f1e25c
Anastasiia Anikina <anikinaanas95@gmail.com>
2024-07-29 19:39:35 +0300

    Add new unit-tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -17,8 +18,87 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    #1. проверка названия -0, 41 символ и более 41 символа - не добавились в словарь
+    @pytest.mark.parametrize('name', ['', 'Смерть приходит за архиепископом, тест, книга, kkk3?','Общая теория занятости, процента и денег!'])
+    def test_add_new_book_add_book_incorrect_characters(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name not in collector.books_genre()
+
+    #2. проверка, что genre присвоился name
+    def test_set_book_genre_add_genre(self):
+        collector = BooksCollector()
+        name = "ОНО"
+        genre = 'Ужасы'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.books_genre[name] == genre
+
+    #3. получить жанр книги по её имени
+    def test_get_book_genre_correct_name(self):
+        collector = BooksCollector()
+        collector.add_new_book("Винни Пух")
+        collector.set_book_genre("Винни Пух", 'Мультфильмы')
+        assert collector.get_book_genre("Винни Пух") == "Мультфильмы"
+
+    #4. проверка, вывода определенного жанра (Детектив) и что в списке сохраняется несколько названий(не перезаписывается)
+    def test_get_books_with_specific_genre_genre_in_list(self):
+        collector = BooksCollector()
+        collector.add_new_book("Маугли")
+        collector.set_book_genre("Маугли", 'Мультфильмы')
+        collector.add_new_book("Шерлок Холмс")
+        collector.set_book_genre("Шерлок Холмс", "Детектив")
+        collector.add_new_book("Убийство в восточном эксперссе")
+        collector.set_book_genre("Убийство в восточном эксперссе", "Детектив")
+        assert collector.get_books_with_specific_genre("Детектив") == ["Шерлок Холмс", "Убийство в восточном эксперссе"]
+
+    #5. Добавить корреткные значения и проверить, что в ответе словарь
+    def test_get_books_genre_check_dict(self):
+        collector = BooksCollector()
+        collector.add_new_book("Золушка")
+        collector.set_book_genre("Золушка", 'Мультфильмы')
+        collector.add_new_book("Десять негритят")
+        collector.set_book_genre("Десять негритят", "Детектив")
+        assert collector.get_books_genre() == {"Золушка": 'Мультфильмы', "Десять негритят": "Детектив"}
+
+    #6. добавить книгу, которая подходит детям
+    def test_get_books_for_children_book_for_child(self):
+        collector = BooksCollector()
+        collector.add_new_book("Маленький принц")
+        collector.set_book_genre("Маленький принц", 'Мультфильмы')
+        collector.add_new_book("Талантливый мистер Рипли")
+        collector.set_book_genre("Талантливый мистер Рипли", "Детектив")
+        assert len(collector.get_books_for_children()) == 1 and collector.get_books_for_children() == ["Маленький принц"]
+
+    #7. Добавить книгу в избранное, проверить что книга добавилась в избранное
+    def test_add_book_in_favorites_book_added(self):
+        collector = BooksCollector()
+        collector.add_new_book("Русалочка")
+        collector.add_new_book("Азазель")
+        collector.add_book_in_favorites("Русалочка")
+        collector.add_book_in_favorites("Азазель")
+        assert collector.favorites == ["Русалочка", "Азазель"]
+
+    #8. Удалить книгу из избранного, проверить что книга не отображается в избранное
+    def test_delete_book_from_favorites_one_book_delete(self):
+        collector = BooksCollector()
+        collector.add_new_book("Приключения Оливера Твиста")
+        collector.add_book_in_favorites("Приключения Оливера Твиста")
+        collector.delete_book_from_favorites("Приключения Оливера Твиста")
+        assert len(collector.favorites) == 0
+
+    #9. Получить список Избранных книг
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Убийство на улице Морг")
+        collector.add_book_in_favorites("Убийство на улице Морг")
+        assert len(collector.get_list_of_favorites_books()) == 1 and collector.get_list_of_favorites_books() == ["Убийство на улице Морг"]
+
+
